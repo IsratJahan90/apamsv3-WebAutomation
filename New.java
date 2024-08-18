d7e3db03421bc686fb6eaae7dba6f2d827fbd272
Israt Jahan <isratjahan299790@gmail.com>
2024-08-18 12:11:33 +0600

    Add files via upload

---
+++ New.java
@@ -0,0 +1,46 @@
+package bd.com.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class New {
+
+	static String Staging = "https://apams-v3.babl.xyz/";
+	static String Live = "https://apamsv3.cabinet.gov.bd/";
+	static String Training = "https://training.apamsv3.cabinet.gov.bd/";
+	WebDriver driver;
+
+	@BeforeSuite
+	public void start() {
+
+
+		if (browser.contains("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		else if (browser.contains("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+
+		else {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+	}
+
+	@AfterSuite
+	public void end() {
+
+		driver.quit();
+
+	}
+	String browser = System.getProperty("browser", "chrome");
+}
---
+++ PageOpen.java
@@ -0,0 +1,161 @@
+package bd.com.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class PageOpen extends New {
+	
+	@Test(priority = 0)
+	public void open() throws InterruptedException {
+		driver.get(Staging);
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+	}
+
+	@Test(priority = 1)
+	public void landing_page() throws InterruptedException {
+		WebElement notice1 = driver
+				.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]"));
+		notice1.click();
+		Thread.sleep(2000);
+
+		WebElement cross = driver.findElement(By.xpath("//body/div[3]/div[1]/div[1]/div[1]/button[1]/i[1]"));
+		cross.click();
+		Thread.sleep(2000);
+
+	}
+
+	@Test(priority = 2)
+	public void login_page() throws InterruptedException {
+		WebElement login = driver
+				.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/button[1]/span[1]"));
+		login.click();
+		Thread.sleep(3000);
+
+		WebElement enothi = driver.findElement(By.xpath(
+				"//body/div[@role='dialog']/div[@class='modal-dialog modal-lg']/div[@class='modal-content']/div[1]/button[1]/h5[1]"));
+		enothi.click();
+		Thread.sleep(2000);
+
+		WebElement userid = driver.findElement(By.xpath("//input[@placeholder='ইউজার আইডি অথবা ইউজার নেম']"));
+		
+		/*userid.sendKeys("2163"); 
+		 * Thread.sleep(1000); 
+		 * userid.clear();
+		 * Thread.sleep(1000);*/
+		
+		userid.sendKeys("200000000163");
+		Thread.sleep(1000);
+
+		WebElement pass = driver.findElement(By.xpath("//input[@id='password']"));
+		pass.sendKeys("02522016");
+		Thread.sleep(1000);
+
+		WebElement click = driver.findElement(By.xpath("//span[@class='mr-1']"));
+		click.click();
+		Thread.sleep(4000);
+
+		/*WebElement username = driver.findElement(By.xpath("//input[@id='email']"));
+		 * username.sendKeys("2163"); 
+		 * Thread.sleep(1000); 
+		 * username.clear();
+		 * Thread.sleep(1000); 
+		 * username.sendKeys("200000000163"); 
+		 * Thread.sleep(1000);
+		 
+		 * WebElement password =
+		 * driver.findElement(By.xpath("//input[@id='password']"));
+		 * password.sendKeys("R3@L$tr0nG!P@$$w0rd"); 
+		 * Thread.sleep(1000);
+		 
+		 * WebElement enothi_login = driver .findElement(By.
+		 * xpath("//h5[contains(text(),'ই-নথি বহির্ভূত ব্যবহারকারী লগইন')]"));
+		 * enothi_login.click(); 
+		 * Thread.sleep(1000);*/
+
+	}
+
+	@Test(priority = 3)
+	public void configuration() throws InterruptedException {
+		WebElement configuration = driver.findElement(By.xpath("//button[contains(text(),'কনফিগারেশন')]"));
+		configuration.click();
+		Thread.sleep(3000);
+
+		WebElement faq = driver.findElement(By.xpath("//a[contains(text(),'এফএকিউ')]"));
+		faq.click();
+		Thread.sleep(3000);
+
+		WebElement new_add = driver.findElement(By.xpath(
+				"//body/div[@id='root']/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/button[1]"));
+		new_add.click();
+		Thread.sleep(3000);
+
+		WebElement ques = driver.findElement(
+				By.xpath("//body/div[3]/div[1]/div[1]/form[1]/div[2]/div[1]/div[1]/div[1]/div[1]/input[1]"));
+		ques.sendKeys("প্রশ্ন");
+		Thread.sleep(3000);
+
+		WebElement ans = driver.findElement(By.xpath(
+				"//body/div[3]/div[1]/div[1]/form[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]"));
+		ans.sendKeys("উত্তর");
+		Thread.sleep(3000);
+
+		WebElement save = driver.findElement(By.xpath("//button[contains(text(),'সংরক্ষণ করুন')]"));
+		save.click();
+		Thread.sleep(5000);
+
+	}
+
+	@Test(priority = 4)
+	public void apa_management() throws InterruptedException {
+
+		WebElement apa_management = driver.findElement(By.xpath("//button[contains(text(),'এপিএ ব্যবস্থাপনা')]"));
+		apa_management.click();
+		Thread.sleep(5000);
+
+		/*
+		 * Select select = new Select(APA_management);
+		 * select.selectByVisibleText("অর্থবছর (তৈরি এবং নির্ধারণ)");
+		 */
+
+		WebElement fiscal_year = driver.findElement(By.xpath("//a[contains(text(),'অর্থবছর (তৈরি এবং নির্ধারণ)')]"));
+		fiscal_year.click();
+		Thread.sleep(2000);
+
+		WebElement fiscal_assign = driver.findElement(By.xpath("//button[contains(text(),'অর্থবছর নির্ধারণ')]"));
+		fiscal_assign.click();
+		Thread.sleep(2000);
+
+		WebElement add = driver.findElement(
+				By.xpath("//body/div[@id='root']/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/button[2]"));
+		add.click();
+		Thread.sleep(2000);
+
+		WebElement fiscal = driver
+				.findElement(By.xpath("//body/div[3]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/select[1]/option[2]"));
+		fiscal.click();
+		Thread.sleep(2000);
+
+		WebElement achieve_year = driver
+				.findElement(By.xpath("//body/div[3]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/select[1]/option[2]"));
+		achieve_year.click();
+		Thread.sleep(2000);
+
+		WebElement layer = driver
+				.findElement(By.xpath("//body/div[3]/div[1]/div[1]/form[1]/div[1]/div[1]/div[3]/select[1]/option[3]"));
+		layer.click();
+		Thread.sleep(2000);
+
+		WebElement drop3 = driver.findElement(
+				By.xpath("//body/div[3]/div[1]/div[1]/form[1]/div[1]/div[1]/div[4]/div[1]/div[2]/input[6]"));
+		drop3.click();
+		Thread.sleep(5000);
+
+		WebElement close = driver.findElement(By.xpath("//button[contains(text(),'সংরক্ষণ করুন')]"));
+		close.click();
+		Thread.sleep(5000);
+
+	}
+
+}
